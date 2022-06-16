86fe34cd21c636e800612106c689cbf3e41ae6e7
Anita279 <anita27991@hotmail.com>
2022-06-16 20:17:35 +0200

    Edited conf file.

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example python'
-copyright = '2022, Alten'
-author = 'Alten'
+PROJECT = 'Example python'
+COPYRIGHT = '2022, Alten'
+AUTHOR = 'Alten'
 
 
 # -- General configuration ---------------------------------------------------
@@ -43,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+HTML_THEME = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
